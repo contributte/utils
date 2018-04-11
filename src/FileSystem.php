3e5fca70aaec9f026dccff28f62822af383169ca
Milan Felix Šulc <rkfelix@gmail.com>
2018-04-11 22:39:34 +0200

    Qa: sniff for tabs

--- src/FileSystem.php
+++ src/FileSystem.php
@@ -12,52 +12,52 @@ use RecursiveIteratorIterator;
 class FileSystem extends NetteFileSystem
 {
 
-    /**
-     * Normalize path
-     *
-     * @param string $path
-     * @return string
-     */
-    public static function pathalize($path)
-    {
-        return str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $path);
-    }
+	/**
+	 * Normalize path
+	 *
+	 * @param string $path
+	 * @return string
+	 */
+	public static function pathalize($path)
+	{
+		return str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $path);
+	}
 
-    /**
-     * Get file extension(.xxx)
-     *
-     * @param string $str
-     * @return string
-     */
-    public static function extension($str)
-    {
-        $pos = strripos($str, '.');
-        if ($pos === FALSE) {
-            return pathinfo($str, PATHINFO_EXTENSION);
-        }
+	/**
+	 * Get file extension(.xxx)
+	 *
+	 * @param string $str
+	 * @return string
+	 */
+	public static function extension($str)
+	{
+		$pos = strripos($str, '.');
+		if ($pos === FALSE) {
+			return pathinfo($str, PATHINFO_EXTENSION);
+		}
 
-        return substr($str, $pos);
-    }
+		return substr($str, $pos);
+	}
 
-    /**
-     * Purges directory
-     *
-     * @param string $dir
-     * @return void
-     */
-    public static function purge($dir)
-    {
-        if (!is_dir($dir)) {
-            mkdir($dir);
-        }
+	/**
+	 * Purges directory
+	 *
+	 * @param string $dir
+	 * @return void
+	 */
+	public static function purge($dir)
+	{
+		if (!is_dir($dir)) {
+			mkdir($dir);
+		}
 
-        foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST) as $entry) {
-            if ($entry->isDir()) {
-                rmdir($entry);
-            } else {
-                unlink($entry);
-            }
-        }
-    }
+		foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST) as $entry) {
+			if ($entry->isDir()) {
+				rmdir($entry);
+			} else {
+				unlink($entry);
+			}
+		}
+	}
 
 }
