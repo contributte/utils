4d589846c90a08a9407242d1a0ff34b9b935060d
MartkCz <antik494@gmail.com>
2019-02-12 08:39:43 +0100

    add Http::metadata

---
+++ src/Http.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types = 1);
+
+namespace Contributte\Utils;
+
+use Nette\StaticClass;
+
+class Http
+{
+
+	use StaticClass;
+
+	/** @var string */
+	private static $metadataPattern = '
+		~<\s*meta\s
+
+		# using lookahead to capture type to $1
+		(?=[^>]*?
+			\b(?:name|property|http-equiv)\s*=\s*
+			(?|"\s*([^"]*?)\s*"|\'\s*([^\']*?)\s*\'|
+			([^"\'>]*?)(?=\s*/?\s*>|\s\w+\s*=))
+		)
+
+		# capture content to $2
+		[^>]*?\bcontent\s*=\s*
+			(?|"\s*([^"]*?)\s*"|\'\s*([^\']*?)\s*\'|
+			([^"\'>]*?)(?=\s*/?\s*>|\s\w+\s*=))
+		[^>]*>
+
+		~ix';
+
+	/**
+	 * Gets http metadata from string
+	 *
+	 * @return string[] [name => content]
+	 */
+	public static function metadata(string $content): array
+	{
+		if (preg_match_all(self::$metadataPattern, $content, $matches)) {
+			return array_combine($matches[1], $matches[2]);
+		}
+
+		return [];
+	}
+
+}
